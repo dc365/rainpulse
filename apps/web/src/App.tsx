7a17913b90f3aa92fe1dc957feedc91a5b1d6eca
hahaha <decailin007@gmail.com>
2026-09-08 14:49:19 +0800

    refactor(web): remove global map probe installation

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,12 +3,9 @@ import { MainWorkspace } from './workspace/MainWorkspace'
 import { PipelineInspector } from './workspace/PipelineInspector'
 import { WorkspaceCrosshairInspector } from './workspace/WorkspaceCrosshairInspector'
 import { WorkspaceLiveBridge } from './workspace/WorkspaceLiveBridge'
-import { installMapProbeBridge } from './workspace/mapProbeBridge'
 import './styles.css'
 import './workspace.css'
 import './workspace-runtime.css'
-
-installMapProbeBridge()
 
 export default function App() {
   return window.location.pathname.startsWith('/admin')
